bd124bde44c7cc86f98170fffc51f877b46d9be1
frankdev7 <frank7rojas@gmail.com>
2023-07-12 08:14:20 -0500

    stages and next

--- pallets/crowdfund/src/tipos.rs
+++ pallets/crowdfund/src/tipos.rs
@@ -2,6 +2,27 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 use frame_support::{traits::Currency, BoundedVec};
+use codec::{Encode, Decode, MaxEncodedLen};
+use scale_info::TypeInfo;
+
+#[derive(Encode, Decode, Debug, PartialEq, Eq, Clone, TypeInfo, MaxEncodedLen)]
+pub enum Stage {
+	NameGeneration,
+  FundCollection,
+  Fulfillment,
+}
+
+impl Stage {
+	pub fn next(&mut self) {
+		use Stage::*;
+
+		*self = match *self {
+			NameGeneration => FundCollection,
+			FundCollection => Fulfillment,
+      Fulfillment => Fulfillment,
+		};
+	}
+}
 
 pub type CuentaDe<T> = <T as frame_system::Config>::AccountId;
 pub type BalanceDe<T> = <<T as crate::Config>::Currency as Currency<CuentaDe<T>>>::Balance;
